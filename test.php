536e7094a88eafd89a63f97721554c8c0d5ae902
Thomas Faurbye Nielsen <archanium@gmail.com>
2012-03-29 09:46:17 +0200

    Starting to figure out how this is supposed to work

--- test.php
+++ test.php
@@ -2,15 +2,32 @@
 
 class wsEpayTest extends PHPUnit_Framework_TestCase
 {
+    /** wsEpay */
     private $wsEpay = null;
     protected function setUp()
     {
         require_once __DIR__ . '/wsEpay.php';
-        $merchantnumber = 0;
-        $this->wsEpay = new wsEpay($merchantnumber/* Missing Merchant number */);
+        $merchantnumber = 8008627;
+        $this->wsEpay = new wsEpay($merchantnumber);
     }
 
-    public function testGetTransactionInformation() {
-        $this->fail('Not implemented');
+    public function testGetTransactionInformationNonExistingTransaction() {
+        $transactId = 3423143242;
+        $transaction = $this->wsEpay->getTransactionInformation($transactId);
+        if(!$transaction) {
+            $this->fail($this->wsEpay->error);
+        }
+
+        $this->assertObjectHasAttribute('gettransactionResult', $transaction, "We didn't get a properly formatted response");
+        $this->assertFalse($transaction->gettransactionResult, "Transaction was successful.");
+
+    }
+
+    public function testCaptureFailure() {
+        $transactId = 3423123123;
+        $amount = 1000;
+        $capture = $this->wsEpay->capture($transactId,$amount);
+        $this->assertObjectHasAttribute('captureResult',$capture,"We didn't get a properly formatted response");
+        $this->assertFalse($capture->captureResult, "The capture was successfull.");
     }
 }
--- wsEpay.php
+++ wsEpay.php
@@ -6,23 +6,29 @@ class wsEpay
     private $client;
     private $encoding;
 
-    public function __construct($merchantnumber, $encoding = 'UTF-8')
+    public function __construct($merchantnumber, $pwd = false, $encoding = 'UTF-8')
     {
-        $this->client = new SoapClient("https://ssl.ditonlinebetalingssystem.dk/remote/payment.asmx?WSDL", array('encoding' => $encoding));
+        $this->client = new SoapClient("https://ssl.ditonlinebetalingssystem.dk/remote/payment.asmx?WSDL", array('encoding' => $encoding, 'trace'  => 1));
         $this->encoding = $encoding;
-        $this->merchantnumber = $merchantnumber;
+        $this->api_params = array(
+            'merchantnumber' => $merchantnumber,
+            'epayresponse' => 1 /* Thanks for messing up everything epay, this value isn't required according to your own implementation? */
+        );
+        if($pwd) {
+            $this->api_params = array_merge($this->api_params,array('pwd' => $pwd));
+        }
+        return $this;
     }
 
     public function getTransactionInformation($transactionId) {
         try
         {
             $params = array(
-                'merchantnumber' => $this->merchantnumber,
-                'transactionid' => $transactionId
-            );
-            $transaction = $this->client->gettransactioninformation($params);
+                'transactionid' => $transactionId,
 
-            return $transaction->GetAllParcelShopsResult->PakkeshopData;
+            );
+            $params = array_merge($this->api_params,$params);
+            return $this->client->gettransaction($params);
         }
         catch (Exception $e)
         {
@@ -30,4 +36,29 @@ class wsEpay
             return false;
         }
     }
+
+    public function capture($transactionId, $amount, $group = null) {
+        try
+        {
+            $params = array(
+                'transactionid' => $transactionId,
+                'amount' => $amount,
+                'pbsResponse' => 1
+            );
+            if(!is_null($group)) {
+                $params = array_merge(array('group' => $group, $params));
+            }
+            $params = array_merge($this->api_params,$params);
+            return $this->client->capture($params);
+        }
+        catch (Exception $e)
+        {
+            $this->error = __METHOD__ . ': ' . $e->getMessage();
+            return false;
+        }
+    }
+
+
+
+
 }
